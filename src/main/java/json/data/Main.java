81baa13d9b3c5b2e562e73fe2fd12940c49f6728
pc <pc@DESKTOP-H6RC7UL>
2018-07-04 11:16:41 +0300

    third commit.

--- src/main/java/json/data/Main.java
+++ src/main/java/json/data/Main.java
@@ -10,150 +10,168 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import models.UserModel;
 import utils.Util;
 
-
-//http://localhost:8080/myProject/webservice/getExample/age/25   
-@Path("/getExample") 
+//http://localhost:8080/myProject/webservice/getExample/age/2
+@Path("/getExample")
 public class Main {
 
-    
-    DbOperations dbOp; 
+    DbOperations dbOp;
     Statement statement;
     public CachedRowSet resWanted;
     Util myUtil;
     String result;
-    
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)           //JSON tipinde bir şeyler üreticem
-    @Path("/getTheList") 
+    @Path("/getTheList")
     public List<UserModel> start() throws Exception {
         ResultSet myRs;
-        dbOp=new DbOperations();
-        myUtil=new Util();
-        
+        dbOp = new DbOperations();
+        myUtil = new Util();
+
         statement = dbOp.baglantiAc();
         resWanted = dbOp.createStatement();
-        myRs=queryWork();
+        myRs = queryWork();
         myUtil.addUser(myRs);
         return myUtil.getUserList();
-        
-        
-    }
-    
-    
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)           //JSON tipinde bir şeyler üreticem
-    @Path("/age/{age}") 
-    public List<UserModel> start(@PathParam("age") int customerAge) throws Exception {
-        ResultSet myRs;
-        dbOp=new DbOperations();
-        myUtil=new Util();
-        
-        statement = dbOp.baglantiAc();
-        resWanted = dbOp.createStatement();
-        myRs=queryWorkByAge(customerAge);
-        myUtil.addUser(myRs);
-        return myUtil.getUserList();
-        
-        
-    }
-    
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)           //JSON tipinde bir şeyler üreticem
-    @Path("/name/{nm}") 
-    public List<UserModel> start(@PathParam("nm") String customerName) throws Exception {
-        ResultSet myRs;
-        dbOp=new DbOperations();
-        myUtil=new Util();
-        
-        statement = dbOp.baglantiAc();
-        resWanted = dbOp.createStatement();
-        myRs=queryWorkByName(customerName);
-        myUtil.addUser(myRs);
-        return myUtil.getUserList();
-        
-        
-    }
-    
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)           //JSON tipinde bir şeyler üreticem
-    @Path("/surname/{sur}") 
-    public List<UserModel> surname(@PathParam("sur") String surname) throws Exception {
-        ResultSet myRs;
-        dbOp=new DbOperations();
-        myUtil=new Util();
-        
-        statement = dbOp.baglantiAc();
-        resWanted = dbOp.createStatement();
-        myRs=queryWorkBySurname(surname);
-        myUtil.addUser(myRs);
-        return myUtil.getUserList();
-        
-        
+
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)           //JSON tipinde bir şeyler üreticem
-    @Path("/id/{id}") 
-    public List<UserModel> doThis(@PathParam("id") int customerID) throws Exception {
+    @Path("/age/{age}")
+    public List<UserModel> start(@PathParam("age") int customerAge) throws Exception {
         ResultSet myRs;
-        dbOp=new DbOperations();
-        myUtil=new Util();
-        
+        dbOp = new DbOperations();
+        myUtil = new Util();
+
         statement = dbOp.baglantiAc();
         resWanted = dbOp.createStatement();
-        myRs=queryWorkByID(customerID);
+        myRs = queryWorkByAge(customerAge);
         myUtil.addUser(myRs);
         return myUtil.getUserList();
-        
-        
+
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)           //JSON tipinde bir şeyler üreticem
+    @Path("/getNameAndAge")
+    public List<UserModel> start(@QueryParam("name") String customerName ,@QueryParam("age") int customerAge) throws Exception {
+        ResultSet myRs;
+        dbOp = new DbOperations();
+        myUtil = new Util();
+
+        statement = dbOp.baglantiAc();
+        resWanted = dbOp.createStatement();
+        myRs = queryWorkByAgeAndName(customerName,customerAge);
+        myUtil.addUser(myRs);
+        return myUtil.getUserList();
+
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)           //JSON tipinde bir şeyler üreticem
+    @Path("/surname/{sur}")
+    public List<UserModel> surname(@PathParam("sur") String surname) throws Exception {
+        ResultSet myRs;
+        dbOp = new DbOperations();
+        myUtil = new Util();
+
+        statement = dbOp.baglantiAc();
+        resWanted = dbOp.createStatement();
+        myRs = queryWorkBySurname(surname);
+        myUtil.addUser(myRs);
+        return myUtil.getUserList();
+
     }
     
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)           //JSON tipinde bir şeyler üreticem
+    @Path("/name/{name}")
+    public List<UserModel> getName(@PathParam("name") String customerName) throws Exception {
+        ResultSet myRs;
+        dbOp = new DbOperations();
+        myUtil = new Util();
+
+        statement = dbOp.baglantiAc();
+        resWanted = dbOp.createStatement();
+        myRs = queryWorkByName(customerName);
+        myUtil.addUser(myRs);
+        return myUtil.getUserList();
+
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)           //JSON tipinde bir şeyler üreticem
+    @Path("/id/{id}")
+    public List<UserModel> doThis(@PathParam("id") int customerID) throws Exception {
+        ResultSet myRs;
+        dbOp = new DbOperations();
+        myUtil = new Util();
+
+        statement = dbOp.baglantiAc();
+        resWanted = dbOp.createStatement();
+        myRs = queryWorkByID(customerID);
+        myUtil.addUser(myRs);
+        return myUtil.getUserList();
+
+    }
+
     public ResultSet queryWork() throws SQLException {
         String query;
         query = "select * from users";
         PreparedStatement pst = dbOp.getConnection().prepareStatement(query);
         ResultSet rs = pst.executeQuery();
         return rs;
-        
+
     }
-    
+
     public ResultSet queryWorkByAge(int age) throws SQLException {
         String query;
         query = "select * from users where age like '" + age + "%'";
         PreparedStatement pst = dbOp.getConnection().prepareStatement(query);
         ResultSet rs = pst.executeQuery();
         return rs;
-        
+
     }
-    
+
     public ResultSet queryWorkByID(int id) throws SQLException {
         String query;
         query = "select * from users where id like '" + id + "%'";
         PreparedStatement pst = dbOp.getConnection().prepareStatement(query);
         ResultSet rs = pst.executeQuery();
         return rs;
-        
+
     }
-    
+
     public ResultSet queryWorkByName(String name) throws SQLException {
         String query;
         query = "select * from users where name like '" + name + "%'";
         PreparedStatement pst = dbOp.getConnection().prepareStatement(query);
         ResultSet rs = pst.executeQuery();
         return rs;
-        
+
     }
-    
+
+    public ResultSet queryWorkByAgeAndName(String name,int age) throws SQLException {
+        String query;
+        query = "select * from users where name like '"+name+"%' and age like '"+age+"%'";
+        PreparedStatement pst = dbOp.getConnection().prepareStatement(query);
+        ResultSet rs = pst.executeQuery();
+        return rs;
+
+    }
+
     public ResultSet queryWorkBySurname(String surname) throws SQLException {
         String query;
         query = "select * from users where surname like '" + surname + "%'";
         PreparedStatement pst = dbOp.getConnection().prepareStatement(query);
         ResultSet rs = pst.executeQuery();
         return rs;
-        
+
     }
 }

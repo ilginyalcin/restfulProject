bad4a5fb4852445d3fbe132190a0e0ec2a9a4b6a
pc <pc@DESKTOP-H6RC7UL>
2018-07-11 08:38:39 +0300

    fourth commit

--- src/main/java/json/data/Main.java
+++ src/main/java/json/data/Main.java
@@ -1,17 +1,25 @@
 package json.data;
 
+import java.io.IOException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 import javax.sql.rowset.CachedRowSet;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import models.UserModel;
 import utils.Util;
 
@@ -33,7 +41,7 @@ public class Main {
         dbOp = new DbOperations();
         myUtil = new Util();
 
-        statement = dbOp.baglantiAc();
+        statement = dbOp.openConnection();
         resWanted = dbOp.createStatement();
         myRs = queryWork();
         myUtil.addUser(myRs);
@@ -49,7 +57,7 @@ public class Main {
         dbOp = new DbOperations();
         myUtil = new Util();
 
-        statement = dbOp.baglantiAc();
+        statement = dbOp.openConnection();
         resWanted = dbOp.createStatement();
         myRs = queryWorkByAge(customerAge);
         myUtil.addUser(myRs);
@@ -60,14 +68,14 @@ public class Main {
     @GET
     @Produces(MediaType.APPLICATION_JSON)           //JSON tipinde bir şeyler üreticem
     @Path("/getNameAndAge")
-    public List<UserModel> start(@QueryParam("name") String customerName ,@QueryParam("age") int customerAge) throws Exception {
+    public List<UserModel> start(@QueryParam("name") String customerName, @QueryParam("age") int customerAge) throws Exception {
         ResultSet myRs;
         dbOp = new DbOperations();
         myUtil = new Util();
 
-        statement = dbOp.baglantiAc();
+        statement = dbOp.openConnection();
         resWanted = dbOp.createStatement();
-        myRs = queryWorkByAgeAndName(customerName,customerAge);
+        myRs = queryWorkByAgeAndName(customerName, customerAge);
         myUtil.addUser(myRs);
         return myUtil.getUserList();
 
@@ -81,14 +89,14 @@ public class Main {
         dbOp = new DbOperations();
         myUtil = new Util();
 
-        statement = dbOp.baglantiAc();
+        statement = dbOp.openConnection();
         resWanted = dbOp.createStatement();
         myRs = queryWorkBySurname(surname);
         myUtil.addUser(myRs);
         return myUtil.getUserList();
 
     }
-    
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)           //JSON tipinde bir şeyler üreticem
     @Path("/name/{name}")
@@ -97,7 +105,7 @@ public class Main {
         dbOp = new DbOperations();
         myUtil = new Util();
 
-        statement = dbOp.baglantiAc();
+        statement = dbOp.openConnection();
         resWanted = dbOp.createStatement();
         myRs = queryWorkByName(customerName);
         myUtil.addUser(myRs);
@@ -106,20 +114,37 @@ public class Main {
     }
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)           //JSON tipinde bir şeyler üreticem
+    @Produces(MediaType.APPLICATION_JSON)
     @Path("/id/{id}")
     public List<UserModel> doThis(@PathParam("id") int customerID) throws Exception {
         ResultSet myRs;
         dbOp = new DbOperations();
         myUtil = new Util();
 
-        statement = dbOp.baglantiAc();
+        statement = dbOp.openConnection();
         resWanted = dbOp.createStatement();
         myRs = queryWorkByID(customerID);
         myUtil.addUser(myRs);
         return myUtil.getUserList();
 
     }
+
+ 
+   /* @POST
+    @Path("/insertUser")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createUser(@Context HttpHeaders,UserModel user) {
+        ResultSet myRs;
+        dbOp = new DbOperations();
+        myUtil = new Util();
+
+        statement = dbOp.openConnection();
+        resWanted = dbOp.createStatement();
+        UserModel mUser=new UserModel(name,surname,age);
+        addANewUser(mUser);
+
+    } */
 
     public ResultSet queryWork() throws SQLException {
         String query;
@@ -157,9 +182,9 @@ public class Main {
 
     }
 
-    public ResultSet queryWorkByAgeAndName(String name,int age) throws SQLException {
+    public ResultSet queryWorkByAgeAndName(String name, int age) throws SQLException {
         String query;
-        query = "select * from users where name like '"+name+"%' and age like '"+age+"%'";
+        query = "select * from users where name like '" + name + "%' and age like '" + age + "%'";
         PreparedStatement pst = dbOp.getConnection().prepareStatement(query);
         ResultSet rs = pst.executeQuery();
         return rs;
@@ -174,4 +199,12 @@ public class Main {
         return rs;
 
     }
+    
+    public void addANewUser(UserModel user) throws SQLException{
+        String query;
+        query = "insert into users (name, surname, age) values ('"+user.getName()+"','"+user.getSurname()+"',"+user.getAge()+")";
+        PreparedStatement pst = dbOp.getConnection().prepareStatement(query);
+        pst.executeUpdate();
+        
+}
 }
